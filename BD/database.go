930f4dea93d650a2900c88db1b354d76d898ad2d
xodox <two.trans@mail.ru>
2026-04-30 19:41:22 +0300

    registr ready

--- BD/database.go
+++ BD/database.go
@@ -11,8 +11,8 @@ func CreateDB(conn *pgx.Conn, ctx context.Context) {
 	sqlQuery := `
 	CREATE TABLE IF NOT EXISTS newTask (
 		id SERIAL PRIMARY KEY,
-		email TEXT, 
-		password TEXT
+		email VARCHAR(255) UNIQUE, 
+		password TEXT UNIQUE
 	);
 	`
 	_, err := conn.Exec(ctx, sqlQuery)
